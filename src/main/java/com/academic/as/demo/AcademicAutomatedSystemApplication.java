c421799521ca31cb7f6f22c39846193b7e549dff
Maxmya <maxmya@outlook.com>
2018-08-11 18:23:02 -0400

    init project

---
+++ src/main/java/com/academic/as/demo/AcademicAutomatedSystemApplication.java
@@ -0,0 +1,12 @@
+package com.academic.as.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicAutomatedSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicAutomatedSystemApplication.class, args);
+    }
+}
