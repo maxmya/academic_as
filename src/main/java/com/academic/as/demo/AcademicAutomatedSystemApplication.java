33ce68ec453b48a049e4d3680093e902b7f91a5b
Maxmya <maxmya@outlook.com>
2018-08-19 22:35:21 -0400

    encode password before store in database

--- src/main/java/com/academic/as/demo/AcademicAutomatedSystemApplication.java
+++ src/main/java/com/academic/as/demo/AcademicAutomatedSystemApplication.java
@@ -2,9 +2,16 @@ package com.academic.as.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AcademicAutomatedSystemApplication {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AcademicAutomatedSystemApplication.class, args);
