2f105ccede9983783288bd3c43417124593a51ec
omarwael <omar_wael1890@yahoo.com>
2018-08-21 22:15:04 +0200

    before pull , omar

--- src/main/java/com/academic/as/demo/AcademicAutomatedSystemApplication.java
+++ src/main/java/com/academic/as/demo/AcademicAutomatedSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AcademicAutomatedSystemApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AcademicAutomatedSystemApplication.class, args);
     }
 }
