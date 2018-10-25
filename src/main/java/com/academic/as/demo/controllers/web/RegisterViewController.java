761453ad245d27d05325fec88867c98dc3e463a5
Mahmoud Aref <maxmya@outlook.com>
2018-10-25 05:36:49 -0400

    fix form

--- src/main/java/com/academic/as/demo/controllers/web/RegisterViewController.java
+++ src/main/java/com/academic/as/demo/controllers/web/RegisterViewController.java
@@ -2,6 +2,8 @@ package com.academic.as.demo.controllers.web;
 
 
 import com.academic.as.demo.models.User;
+import com.academic.as.demo.services.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class RegisterViewController {
 
+    @Autowired
+    RegisterService registerService;
+
     @GetMapping("/")
     public String index(Model model) {
         return "index";
@@ -18,14 +23,14 @@ public class RegisterViewController {
 
     @GetMapping("/register")
     public String registerUserView(Model model) {
-        model.addAttribute("user", new User());
+        model.addAttribute(new User());
         return "add_user";
     }
 
     @PostMapping("/register")
-    public String registerUser(Model model,@ModelAttribute("user") User user) {
-        System.out.println(user.getEmail());
-        return "index";
+    public String registerUser(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("response", registerService.addUser(user));
+        return "home";
     }
 
 }
