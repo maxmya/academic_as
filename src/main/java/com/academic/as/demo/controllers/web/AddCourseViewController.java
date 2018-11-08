a71eb1e18ed14e8963208a57637864c6bd44d0fd
Omar Wael <omar_wael1890@yahoo.com>
2018-11-08 12:51:31 +0200

    course validation

--- src/main/java/com/academic/as/demo/controllers/web/AddCourseViewController.java
+++ src/main/java/com/academic/as/demo/controllers/web/AddCourseViewController.java
@@ -1,23 +1,21 @@
 package com.academic.as.demo.controllers.web;
 
 import com.academic.as.demo.models.Course;
-import com.academic.as.demo.models.Department;
 import com.academic.as.demo.services.CoursesService;
 import com.academic.as.demo.services.SpecializationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.validation.Valid;
 
 @Controller
-public class AddCourseViewController {
+public class AddCourseViewController implements WebMvcConfigurer {
 
     @Autowired
     CoursesService coursesService;
@@ -25,17 +23,25 @@ public class AddCourseViewController {
     @Autowired
     SpecializationService specializationService;
 
-    @GetMapping("/addcoursee")
+    @GetMapping("/add/course")
     public String addCourseView(Model model) {
         model.addAttribute(new Course());
         model.addAttribute("departments", specializationService.getAllDepartments().getData());
-        return "addcourse";
+        return "add_course";
     }
 
-    @PostMapping("/addcoursee")
-    public String addCourse(@ModelAttribute("course") Course course, Model model) {
+    @PostMapping("/add/course")
+    public String addCourse(@ModelAttribute("course") @Valid Course course, BindingResult bindingResult , Model model) {
+
+        System.out.print("errors");
+        System.out.print(bindingResult.getAllErrors());
+        if (bindingResult.hasErrors()) {
+            return "add_course";
+        }
+//TODO redirect to the same page
         model.addAttribute("response", coursesService.addCourseByDepName(course));
-        return "home";
+        model.addAttribute("departments", specializationService.getAllDepartments().getData());
+        return "add_course";
     }
 
 
